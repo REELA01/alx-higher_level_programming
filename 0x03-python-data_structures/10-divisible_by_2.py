9c442ec31e6d1e8f50d41b73b7b42a726dba1199
reela <sudanrela123@gmail.com>
2023-12-03 04:20:52 -0800

    third

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+def divisible_by_2(my_list=[]):
+    if my_list is None:
+        return None
+    lis = []
+    for i in my_list:
+        if i % 2 == 0:
+            lis.append(True)
+        else:
+            lis.append(False)
+    return lis
+
