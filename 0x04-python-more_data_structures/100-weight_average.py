75523d2c4ad5b3d8cafa35e1ee80d5041327aac6
reela <sudanrela123@gmail.com>
2023-12-06 11:03:08 -0800

    third

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,8 +3,8 @@ def weight_average(my_list=[]):
     if not isinstance(my_list, list) or len(my_list) == 0:
         return (0)
     avg = 0
-    base = 0
-    for i in my_list:
-        avg += (i[0] * i[1])
-        base += i[1]
-    return (avg / base)
+    size = 0
+    for tup in my_list:
+        avg += (tup[0] * tup[1])
+        size += tup[1]
+    return (avg / size)
