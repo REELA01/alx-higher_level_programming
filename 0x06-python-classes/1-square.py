ab1fbf247a6a45f71d8eda8a542ec65cebd9c559
reela <sudanrela123@gmail.com>
2023-12-19 05:12:37 -0800

    first

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square thing"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """inilization
+        Args:
+            size: square size
+        """
+        self.__size = size
