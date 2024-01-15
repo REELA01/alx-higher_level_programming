ee95825793ffdfb1315162c517482003a64f0664
reela <sudanrela123@gmail.com>
2024-01-15 07:26:16 -0800

    last

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""defines the Base class"""
+"""defines base class"""
 from json import dumps, loads
 import csv
 
 
 class Base:
-    """the base class representation"""
+    '''A representation of the base of our OOP hierarchy.'''
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """initlization"""
+        '''Constructor.'''
         if id is not None:
             self.id = id
         else:
@@ -19,7 +19,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """JSON string representation of list_dictionaries"""
+        '''Jsonifies a dictionary so it's quite rightly and longer.'''
         if list_dictionaries is None or not list_dictionaries:
             return "[]"
         else:
@@ -27,22 +27,22 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """save json represnation to a file"""
+        '''Saves jsonified object to file.'''
         if list_objs is not None:
-            list_objs = [ob.to_dictionary() for ob in list_objs]
+            list_objs = [o.to_dictionary() for o in list_objs]
         with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(list_objs))
 
     @staticmethod
     def from_json_string(json_string):
-        """deserialaization"""
+        '''Unjsonifies a dictionary.'''
         if json_string is None or not json_string:
             return []
         return loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """returns an instance with all attributes already set"""
+        '''Loads instance from dictionary.'''
         from models.rectangle import Rectangle
         from models.square import Square
         if cls is Rectangle:
@@ -56,26 +56,26 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        """load from the file that we have save to"""
+        '''Loads string from file and unjsonifies.'''
         from os import path
         file = "{}.json".format(cls.__name__)
         if not path.isfile(file):
             return []
         with open(file, "r", encoding="utf-8") as f:
-            return [cls.create(**dic) for dic in cls.from_json_string(f.read())]
+            return [cls.create(**d) for d in cls.from_json_string(f.read())]
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """save to csv"""
+        '''Saves object to csv file.'''
         from models.rectangle import Rectangle
         from models.square import Square
         if list_objs is not None:
             if cls is Rectangle:
-                list_objs = [[ob.id, ob.width, ob.height, ob.x, ob.y]
-                             for ob in list_objs]
+                list_objs = [[o.id, o.width, o.height, o.x, o.y]
+                             for o in list_objs]
             else:
-                list_objs = [[ob.id, ob.size, ob.x, ob.y]
-                             for ob in list_objs]
+                list_objs = [[o.id, o.size, o.x, o.y]
+                             for o in list_objs]
         with open('{}.csv'.format(cls.__name__), 'w', newline='',
                   encoding='utf-8') as f:
             writer = csv.writer(f)
@@ -83,7 +83,7 @@ class Base:
 
     @classmethod
     def load_from_file_csv(cls):
-        """load from csv"""
+        '''Loads object to csv file.'''
         from models.rectangle import Rectangle
         from models.square import Square
         ret = []
@@ -93,12 +93,12 @@ class Base:
             for row in reader:
                 row = [int(r) for r in row]
                 if cls is Rectangle:
-                    dic = {"id": row[0], "width": row[1], "height": row[2],
+                    d = {"id": row[0], "width": row[1], "height": row[2],
                          "x": row[3], "y": row[4]}
                 else:
-                    dic = {"id": row[0], "size": row[1],
+                    d = {"id": row[0], "size": row[1],
                          "x": row[2], "y": row[3]}
-                ret.append(cls.create(**dic))
+                ret.append(cls.create(**d))
         return ret
 
     @staticmethod
@@ -108,21 +108,21 @@ class Base:
         from random import randrange
         turtle.Screen().colormode(255)
         for i in list_rectangles + list_squares:
-            tur = turtle.Turtle()
-            tur.color((randrange(255), randrange(255), randrange(255)))
-            tur.pensize(1)
-            tur.penup()
-            tur.pendown()
-            tur.setpos((i.x + tur.pos()[0], i.y - tur.pos()[1]))
-            tur.pensize(10)
-            tur.forward(i.width)
-            tur.left(90)
-            tur.forward(i.height)
-            tur.left(90)
-            tur.forward(i.width)
-            tur.left(90)
-            tur.forward(i.height)
-            tur.left(90)
-            tur.end_fill()
+            t = turtle.Turtle()
+            t.color((randrange(255), randrange(255), randrange(255)))
+            t.pensize(1)
+            t.penup()
+            t.pendown()
+            t.setpos((i.x + t.pos()[0], i.y - t.pos()[1]))
+            t.pensize(10)
+            t.forward(i.width)
+            t.left(90)
+            t.forward(i.height)
+            t.left(90)
+            t.forward(i.width)
+            t.left(90)
+            t.forward(i.height)
+            t.left(90)
+            t.end_fill()
 
         time.sleep(5)
