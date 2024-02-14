6dacd5d8387c060c6ad98cc4959a7d4cdc92eea6
reela <sudanrela123@gmail.com>
2024-02-14 07:23:35 -0800

    first

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- cript that creates the database hbtn_0d_usa and the table states in the database hbtn_0d_usa
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'states' (id UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL, name VARCHAR(256) NOT NULL);
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL, name VARCHAR(256) NOT NULL);
