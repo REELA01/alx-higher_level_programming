21ad57ae51ae50d7432d8934a00ef2d2761e1f12
reela <sudanrela123@gmail.com>
2024-03-15 07:08:17 -0700

    test

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""list all states from database hbtn_0e_0_usa """
+"""list states from database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    dbsql = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], dbsql=sys.argv[3], port=3306)
-    curr = dbsql.cursor()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    curr = db.cursor()
     curr.execute("""SELECT * FROM states WHERE name
                 LIKE BINARY 'N%' ORDER BY states.id""")
     ro = curr.fetchall()
-    for row in ro:
-        print(row)
+    for r in ro:
+        print(r)
     curr.close()
-    dbsql.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""list all states from database hbtn_0e_0_usa """
+"""list states from database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    dbsql = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], dbsql=sys.argv[3], port=3306)
-    curr = dbsql.cursor()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    curr = db.cursor()
     curr.execute("""SELECT cities.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id
                 WHERE states.name=%s""", (sys.argv[4],))
     ro = curr.fetchall()
-    cur = list(row[0] for row in ro)
+    cur = list(r[0] for r in ro)
     print(*cur, sep=", ")
     curr.close()
-    dbsql.close()
+    db.close()
