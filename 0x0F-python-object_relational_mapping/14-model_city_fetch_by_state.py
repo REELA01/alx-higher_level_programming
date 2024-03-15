607826d21542de7cc7979a3f7ceff96c6add132a
reela <sudanrela123@gmail.com>
2024-03-15 07:39:10 -0700

    test

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     se1 = Session()
-    for ins1 in (se1.query(State.name, City.id, City.name)
+    for instance in (se1.query(State.name, City.id, City.name)
                      .filter(State.id == City.state_id)):
-        print(ins1[0] + ": (" + str(ins1[1]) + ") " + ins1[2])
+        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
