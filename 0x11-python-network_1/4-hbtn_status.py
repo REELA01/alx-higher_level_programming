f6d8216f81c7eb79b076231dc8bd77d63eae1f68
reela <sudanrela123@gmail.com>
2024-03-28 07:53:25 -0700

    my

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """script that fetches https://alx-intranet.hbtn.io/status"""
-import requests
 
 
-if __name__ == "__main__":
-    req = requests.get("https://intranet.hbtn.io/status")
+if __name__ == '__main__':
+    import requests
+
+    req = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(req.text.__class__))
     print("\t- content: {}".format(req.text))
